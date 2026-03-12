5d991e109019e5633e78554753667f3bc6060e57
RudraNarayan2005 <rudranarayanrout2005@gmail.com>
2026-03-12 23:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -322,6 +322,7 @@ def autocomplete():
 # ==============================
 # RUN APP
 # ==============================
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
