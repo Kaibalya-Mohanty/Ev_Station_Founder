ee6b988765eaa47cb9256a9b91b9fb0f39f97220
Shreya Dash <157912871+its-shreyaDEV@users.noreply.github.com>
2026-03-25 16:31:43 +0530

    Refactor database for better readability and structure
    Updated comments for clarity and added a note about the connection object.

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
+#file base db
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect("users.db")   # This creates a permanent file
+    conn = sqlite3.connect("users.db")   # Creates or connects to the database file
+   #conn object the bridge btw py and db
+    #cursor → tool to run queries
     c = conn.cursor()
 
+    #It avoids error when the table already exists
     c.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
